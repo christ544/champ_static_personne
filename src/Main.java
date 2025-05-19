c29ff04d2ca6806f5abb3df49a98f5faa941853d
yyyd <yyd291205@gmail.com>
2025-05-19 13:12:35 +0000

    premier commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Personne personne1 = new Personne();
+        Personne personne2 = new Personne();
+        Personne personne3 = new Personne();
+        Personne personne4 = new Personne();
+        // Affichez les valeurs des deux compteurs
+        System.out.println("(" + personne2.nbLocal + "," +
+                Personne.nbInstances + ")");
+    }
+}
